0b06ea9be85ba91e3ee5e9e3b92cb59a5d4b0d56
4lysson-a <4ly.alcantara@gmail.com>
2022-01-10 00:09:47 -0300

    better path structure

--- src/App.js
+++ src/App.js
@@ -1,47 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
-import data from "./data/pratos";
-import Prato from "./components/prato/Prato";
+import Home from "./pages/Home";
 import Header from "./components/header/Header";
 
 import "./style/global.css";
 
 export default function App() {
-  const [pratoState, setPratoState] = useState([]);
-
-  useEffect(() => {
-    // oqq acontece
-    setPratoState(data);
-  }, []); // Array de dependências
-
-  const handleCompra = (id) => {
-    const prato = pratoState.find((item) => item.id === id);
-    prato.comprou = !prato.comprou || false;
-    setPratoState([...pratoState]);
-  };
-
   return (
     <>
       <Header />
-      <div className="App">
-        <div className="pratos">
-          <div className="pratosContainer">
-            {pratoState.map((item) => {
-              return (
-                <Prato
-                  key={item.id}
-                  id={item.id}
-                  comprar={handleCompra}
-                  titulo={item.descricao}
-                  imagem={item.imagem}
-                  preco={item.preco}
-                  comprou={item.comprou}
-                />
-              );
-            })}
-          </div>
-        </div>
-      </div>
+      <Home />
     </>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+
+import data from "../data/pratos";
+import Prato from "../components/prato/Prato";
+
+import "./home.css"
+
+function Home() {
+  const [pratoState, setPratoState] = useState([]);
+
+  useEffect(() => {
+    // oqq acontece
+    setPratoState(data);
+  }, []); // Array de dependências
+
+  const handleCompra = (id) => {
+    const prato = pratoState.find((item) => item.id === id);
+    prato.comprou = !prato.comprou || false;
+    setPratoState([...pratoState]);
+  };
+
+  return (
+    <>
+      <div className="App">
+        <div className="pratos">
+          <div className="pratosContainer">
+            {pratoState.map((item) => {
+              return (
+                <Prato
+                  key={item.id}
+                  id={item.id}
+                  comprar={handleCompra}
+                  titulo={item.descricao}
+                  imagem={item.imagem}
+                  preco={item.preco}
+                  comprou={item.comprou}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home
