166d58abace212f0cd8bb525fd9d350b9e369870
Guilherme Santos <santosguilhermefk@gmail.com>
2022-01-29 23:58:24 -0300

    ajustando detalhes e removendo variaveis inutéis

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import { useState } from "react/cjs/react.development";
 import * as styled from "./style";
 
@@ -7,34 +7,25 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { DataContext } from "../../contexts/DataContext";
 
 function Login() {
-  const {dataUser} = useContext(DataContext)
+  const { dataUser } = useContext(DataContext);
   const [logar, setLogar] = useState(false);
   const [receiveUser, setReceiveUser] = useState("");
   const [passwordReceive, setPasswordReceive] = useState("");
-  
-  
 
-
-console.log(dataUser)
+  console.log(dataUser);
   function validate() {
-    Object.keys(dataUser).forEach((key,value) => {
-
-      const users = [(dataUser[key])]; 
+    Object.keys(dataUser).forEach((key, value) => {
+      const users = [dataUser[key]];
       const usersName = users[0].nome;
-      const usersPassword = users[0].senha
-      if (usersName === receiveUser && usersPassword === passwordReceive){
-        setLogar(true)
-
-      } else{
-        alert('dados incorretos')
+      const usersPassword = users[0].senha;
+      if (usersName === receiveUser && usersPassword === passwordReceive) {
+        setLogar(true);
+      } else {
+        alert("dados incorretos");
       }
-     
     });
-  
-  
-   
   }
-  
+
   <Router>
     <Routes>
       <Route path="home" element={<Home />} />
--- src/pages/signin/Signin.js
+++ src/pages/signin/Signin.js
@@ -1,21 +1,16 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import { useForm } from "react-hook-form";
-import { useState } from "react/cjs/react.development";
 import { DataContext } from "../../contexts/DataContext";
 
-
 function Signin() {
-  const {setDataUser} = useContext(DataContext)
-  
+  const { setDataUser } = useContext(DataContext);
 
   const { register, handleSubmit } = useForm();
   const onSubmit = (data) => {
-   const dataReceive = [data]
-    console.log(dataReceive)
+    const dataReceive = [data];
+    console.log(dataReceive);
     setDataUser(dataReceive);
   };
-
-
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
