40864b0bc2d701c645e2d2d76d87a9e5cd95a3ba
Guilherme Santos <santosguilhermefk@gmail.com>
2022-02-17 13:54:09 -0300

    changing header routes

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,41 +1,42 @@
-import React,{useState} from 'react'
-import { BsFillCartFill as BuyCar } from 'react-icons/bs';
-import { FaUserCircle as UserIcon } from 'react-icons/fa';
-import Modal from "../modal/Modal"
+import React, { useState } from "react";
+import { BsFillCartFill as BuyCar } from "react-icons/bs";
+import { FaUserCircle as UserIcon } from "react-icons/fa";
+import Modal from "../modal/Modal";
 import { Link } from "react-router-dom";
-import Cart from "../cartContainer/Cart"
-import {HeaderContainer}  from "./style"
-import {TitleContainer} from "./style"
-import {NavigationBar} from "./style"
-import {UserContainer} from "./style"
+import Cart from "../cartContainer/Cart";
+import { HeaderContainer } from "./style";
+import { TitleContainer } from "./style";
+import { NavigationBar } from "./style";
+import { UserContainer } from "./style";
 
 function Header() {
-  const [openModal,setOpenModal] = useState(false)
-  const [openCart,setOpenCart] = useState(false)
+  const [openModal, setOpenModal] = useState(false);
+  const [openCart, setOpenCart] = useState(false);
 
   return (
     <>
-    {openCart && (<Cart setOpenCart={setOpenCart}/>)}
-    {openModal && ( <Modal setOpen={setOpenModal}/>)};
-   
+      {openCart && <Cart setOpenCart={setOpenCart} />}
+      {openModal && <Modal setOpen={setOpenModal} />};
       <HeaderContainer>
-       <TitleContainer>
-          <h1 >App de comida do gordo</h1>
-          <h2 > coma hoje pague ontem </h2>
-          </TitleContainer>
+        <TitleContainer>
+          <h1>App de comida do gordo</h1>
+          <h2> coma hoje pague ontem </h2>
+        </TitleContainer>
         <NavigationBar>
-        <Link className="links" to="/">Home</Link> 
-        <Link className="links" to="foods">Foods</Link>
-        
+          <Link className="links" to="home">
+            Home
+          </Link>
+          <Link className="links" to="foods">
+            Foods
+          </Link>
         </NavigationBar>
-      
+
         <UserContainer>
-        <BuyCar className='cart' onClick={() => setOpenCart(true)}/>
-        <UserIcon className='icon' onClick={() => setOpenModal(true)}/>
+          <BuyCar className="cart" onClick={() => setOpenCart(true)} />
+          <UserIcon className="icon" onClick={() => setOpenModal(true)} />
         </UserContainer>
-        </HeaderContainer>
-        
+      </HeaderContainer>
     </>
-  )
+  );
 }
 export default Header;
